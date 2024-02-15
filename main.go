29627997a69f71d62eb5ad5f16c4ef2fd7702cdd
iJoyRide <sumfeis3@gmail.com>
2024-02-15 21:40:05 +0800

    commit

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("6863492345:AAH-ak_depbfolBuCoI7PzfHu4ajJZ0L030"))
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// Now you can retrieve the TELEGRAM_APITOKEN from environment
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error creating bot: %v", err)
 	}
 
 	bot.Debug = true
+	// Your bot's setup and logic go here...
 }
