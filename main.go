2dc25a70a51338b24693715148adad89eee60696
sumfei <117502525+iJoyRide@users.noreply.github.com>
2024-02-19 15:20:49 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,1 @@
-package main
 
-import (
-	init "github.com/iJoyRide/bridge/init"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
-)
-
-func main() {
-	bot, err := tgbotapi.NewBotAPI("TELEGRAM_APITOKEN")
-	if err != nil {
-		panic(err)
-	}
-
-	bot.Debug = true
-
-	init.InitializeGame(bot)
-}
